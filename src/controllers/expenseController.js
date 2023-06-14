b2252210943c7a5f25e1fc10f94471ac15ef8811
sandrorzimmer <sandrorzimmer@gmail.com>
2023-06-13 21:21:25 -0300

    Created more expenses search options
    "/expenses/search/:year"
    "/expenses/search/:year/:month/:day"
    
    Created a function to deal with start and end dates.
    
    Set start date for search with time 00:00:00.000Z and end date for search with time 23:59:59.999Z to cover the whole day peeriod.

--- src/controllers/expenseController.js
+++ src/controllers/expenseController.js
@@ -2,6 +2,7 @@ import NotFound from "../errors/NotFound.js";
 import Category from "../models/Category.js";
 import PaymentMethod from "../models/PaymentMethod.js";
 import Expense from "../models/Expense.js";
+import BadRequest from "../errors/BadRequest.js";
 
 class ExpenseController {
     static showExpenses = async (req, res, next) => {
@@ -121,12 +122,11 @@ class ExpenseController {
         }
     }
 
-    static showExpensesByYearMonth = async (req, res, next) => {
+    static showExpensesByYear = async (req, res, next) => {
         try {
-            const targetYear = req.params.year;
-            const targetMonth = req.params.month;
+            const targetYear = parseInt(req.params.year);
 
-            const [startDate, endDate] = getRangeDate(targetYear, targetMonth);
+            const [startDate, endDate] = getRangeDate(targetYear);
 
             const searchExpenses = Expense
                 .find({
@@ -139,6 +139,59 @@ class ExpenseController {
             req.result = searchExpenses;
 
             next();
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    static showExpensesByYearMonth = async (req, res, next) => {
+        try {
+            const targetYear = parseInt(req.params.year);
+            const targetMonth = parseInt(req.params.month);
+
+            if (getRangeDate(targetYear, targetMonth) === false) {
+                next(new BadRequest("Date is not valid"))
+            } else {
+                const [startDate, endDate] = getRangeDate(targetYear, targetMonth);
+
+                const searchExpenses = Expense
+                    .find({
+                        date: {
+                            $gte: startDate,
+                            $lte: endDate
+                        }
+                    });
+                req.result = searchExpenses;
+
+                next();
+            }
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    static showExpensesByYearMonthDay = async (req, res, next) => {
+        try {
+            const targetYear = parseInt(req.params.year);
+            const targetMonth = parseInt(req.params.month);
+            const targetDay = parseInt(req.params.day);
+
+            if (getRangeDate(targetYear, targetMonth, targetDay) === false) {
+                next(new BadRequest("Date is not valid"))
+            } else {
+                const [startDate, endDate] = getRangeDate(targetYear, targetMonth, targetDay);
+
+                const searchExpenses = Expense
+                    .find({
+                        date: {
+                            $gte: startDate,
+                            $lte: endDate
+                        }
+                    });
+                req.result = searchExpenses;
+
+                next();
+            }
         } catch (error) {
             next(error);
         }
@@ -164,12 +217,78 @@ class ExpenseController {
     }
 }
 
-//Return month and year to search
-function getRangeDate(year, month) {
-    const startDate = new Date(year, month - 1, 1); //first day of the month
-    const endDate = new Date(year, month, 0); //last day of the month
+//Returns an array with start date and end date, in ISO format.
+//Accepts:
+//(year, month, day) --> one day range
+//(year, month) --> one month range
+//(year) --> one year range
+//Start date has time set to 00:00:00.000Z
+//End date has time set to 23:59:59.999Z
+//If provided data is an invalid date, returns false.
+function getRangeDate(year, month, day) {
+    let startDate;
+    let endDate;
 
-    return [startDate, endDate];
+    try {
+
+        //Only year is provided
+        if (month === undefined && day === undefined) {
+            startDate = (new Date(parseInt(year), 0, 1)).toISOString();
+            endDate = (new Date(parseInt(year), 11, 31)).toISOString();
+
+            //Year and month are provided
+        } else if (day === undefined) {
+            if (checkDateIsValid(`${year}-${month}-01`) === false) {
+                return false;
+            } else {
+                startDate = (new Date(parseInt(year), parseInt(month) - 1, 1)).toISOString();
+                endDate = (new Date(parseInt(year), parseInt(month), 0)).toISOString();
+            }
+
+            //Year, month and day are provided
+        } else {
+            if (checkDateIsValid(`${year}-${month}-${day}`) === false) {
+                return false;
+            } else {
+                startDate = new Date(parseInt(year), parseInt(month) - 1, parseInt(day)).toISOString();
+                endDate = startDate;
+            }
+        }
+    } catch (error) {
+        return false;
+    }
+
+    const stringStartDate = startDate.slice(0, 11) + '00:00:00.000Z'; // Set time to 00:00:00.000Z
+    const stringEndDate = endDate.slice(0, 11) + '23:59:59.999Z'; // Set time to 23:59:59.999Z
+
+    const newStartDate = new Date(stringStartDate);
+    const newEndDate = new Date(stringEndDate);
+
+    return [newStartDate, newEndDate];
+}
+
+//Check if date is valid
+//Input format: ("yyyy-mm-dd")
+function checkDateIsValid(date) {
+    const [year, month, day] = date.split('-');
+    const parsedYear = parseInt(year);
+    const parsedMonth = parseInt(month) - 1; // Adjust month to be zero-based
+    const parsedDay = parseInt(day);
+
+    const targetDate = new Date(parsedYear, parsedMonth, parsedDay);
+
+    // Check if the parsed components match the original date and fall within the expected ranges
+    if (
+        parsedYear !== targetDate.getFullYear() ||
+        parsedMonth !== targetDate.getMonth() ||
+        parsedDay !== targetDate.getDate() ||
+        parsedMonth < 0 || parsedMonth > 11 ||
+        parsedDay < 1 || parsedDay > 31
+    ) {
+        return false;
+    }
+
+    return targetDate.toISOString();
 }
 
 //Return array with current year and current month (1 - 12)
--- src/routes/expensesRoutes.js
+++ src/routes/expensesRoutes.js
@@ -7,7 +7,9 @@ const router = express.Router();
 router
     .get("/expenses", ExpenseController.showExpenses, paginate)
     .get("/expenses/search", ExpenseController.showExpensesByFilter, paginate)
+    .get("/expenses/search/:year", ExpenseController.showExpensesByYear, paginate)
     .get("/expenses/search/:year/:month", ExpenseController.showExpensesByYearMonth, paginate)
+    .get("/expenses/search/:year/:month/:day", ExpenseController.showExpensesByYearMonthDay, paginate)
     .get("/expenses/currentMonth", ExpenseController.showExpensesCurrentMonth, paginate)
     .get("/expenses/lastMonth", ExpenseController.showExpensesLastMonth, paginate)
     .get("/expenses/:id", ExpenseController.showExpenseById)
